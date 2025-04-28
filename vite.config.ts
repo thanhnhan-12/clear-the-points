f8e7fcc2aac354b4f020e4dc14d8a9037964f931
thanhnhan-12 <nguyenthanhnhan12701@gmail.com>
2025-04-28 21:53:54 +0700

    add vite path

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_PATH_CLEAR_POINT || "/clear-the-points"
 })
